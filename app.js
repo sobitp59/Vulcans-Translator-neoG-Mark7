93489fa2d0fc4e138287be064b9efa4d1bb800d6
Sobit Prasad <sobitp59@gmail.com>
2022-03-05 23:51:34 +0530

    app.js done

--- app.js
+++ app.js
@@ -1,28 +1,67 @@
-const inputArea = document.querySelector('.input-area');
-const outputArea = document.querySelector('.output-area');
-const btn = document.querySelector('.btn-translate');
+const btn = document.querySelector('.speakBtn');
+const input = document.querySelector('.input');
+const output = document.querySelector('.output');
 
 
-const url = 'https://api.funtranslations.com/translate/doge.json';
 
-function serverUrl(text){
-    return url + '?text=' + text;
-}
 
-function errorHandler(error){
-    alert(`something wrong with the server! Try again after sometime.`)
-}
-
-const translated = () => {
-    let engInput = inputArea.value;
+btn.addEventListener('click', function(){
+   recognition.start();
+   console.log('start')
    
-    fetch(serverUrl(engInput))
-    .then(res => res.json())
-    .then(json =>{
-        outputArea.innerText =  json.contents.translated;
-    }) 
-    .catch(errorHandler)
-        
-}
-btn.addEventListener('click', translated);
+   
+   recognition.onresult = function(event){
+       const current = event.resultIndex;
+       const transcriptInput = event.results[current][0].transcript;
+       input.textContent = transcriptInput;
+       
+       
+       
+    
+    const url = 'https://api.funtranslations.com/translate/vulcan.json';
 
+    function serverUrl(text){
+        return url + '?text=' + text;
+    }
+
+    function errorHandler(error){
+        alert(`something wrong with the server! Try again after sometime. ${error.message}`);
+    }
+    
+    const translated = () => {
+        let inputData = transcriptInput;   
+        fetch(serverUrl(inputData))
+        .then(res => res.json())
+        .then(json => {
+            let translateOut = json.contents.translated;
+            readOutLoud(translateOut);
+            output.innerText = translateOut;
+        }) 
+        .catch(errorHandler)
+            
+    }
+translated();
+}   
+})
+
+
+
+//-------------------- SPEECH -----------------------//
+
+// Speech Recognition
+var SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+
+// Speech Synthesizer
+function readOutLoud(message){
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = message;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+
+    window.speechSynthesis.speak(speech);
+}
+// ---------------------- SPEECH ---------------------------//
