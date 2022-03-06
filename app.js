e894338f348c68dd8d446670f399a666565cb68a
Sobit Prasad <sobitp59@gmail.com>
2022-03-06 08:38:35 +0530

    refactored

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const btn = document.querySelector('.speakBtn');
 const input = document.querySelector('.input');
 const output = document.querySelector('.output');
 
+// Speech Recognition
+var SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+
+// Speech Synthesizer
+function readOutLoud(message){
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = message;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    window.speechSynthesis.speak(speech);
+}
 
 btn.addEventListener('click', function(){
 
@@ -41,17 +55,3 @@ translated();
 }   
 });
 
-// Speech Recognition
-var SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
-var recognition = new SpeechRecognition();
-
-
-// Speech Synthesizer
-function readOutLoud(message){
-    const speech = new SpeechSynthesisUtterance();
-    speech.text = message;
-    speech.volume = 1;
-    speech.rate = 1;
-    speech.pitch = 1;
-    window.speechSynthesis.speak(speech);
-}
